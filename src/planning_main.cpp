c978746dc74077cb441aee438862c922c1b0de44
CXY <2630747598@qq.com>
2025-02-25 20:08:18 +0800

    add easyx

--- src/planning_main.cpp
+++ src/planning_main.cpp
@@ -1,5 +1,6 @@
 #include "process.h"
 #include <iostream>
+#include "show_result.h"
 using namespace std;
 int main()
 {
@@ -7,5 +8,10 @@ int main()
     Process my_process;
     my_process.planProcess();
     cout << "plan end" << endl;
+
+    cout << "show result" << endl;
+    ShowResult my_show_result;
+    my_show_result.drawResult();
+    cout << "show end" << endl;
     return 0;
 }
